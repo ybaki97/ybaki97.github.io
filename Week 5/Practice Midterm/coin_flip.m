0adf1d1b3dbe26efc75a67b943cee9c8686510f6
Yasmeen Baki <90074251+ybaki97@users.noreply.github.com>
2022-11-04 11:22:50 -0700

    Delete Week 5 directory

--- Week 5/Practice Midterm/coin_flip.m
+++
@@ -1,7 +0,0 @@
-function [s] = coin_flip( )
-x = rand();
-if x <= 0.51
-    s = "heads";
-elseif x > 0.51
-    s = "tails";
-end
--- Week 5/Practice Midterm/dirichlet.m
+++
@@ -1,7 +0,0 @@
-function [p] = dirichlet(a,s)
-n = 0;
-while ~isprime(a + s*n)
-    disp(a + s*n)
-    n = n + 1;
-end
-p = a + s*n;
